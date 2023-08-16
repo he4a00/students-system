d1d85012a267f817f9f4454defc9588d3f84bc38
he4a00 <he4a00@github.com>
2023-08-16 17:35:59 +0300

    api domain

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -22,7 +22,7 @@ if (typeof window !== "undefined") {
 }
 
 const api = axios.create({
-  baseURL: "http://localhost:9090/api/",
+  baseURL: "https://students-system-api-isyj.onrender.com/api/",
   headers: {
     Authorization: parsedToken ? `Bearer ${parsedToken}` : undefined,
   },
